909c3dfd1d9586dfd0bc37c9c7c283b70c034178
ArtLavr <onemurder39@gmail.com>
2020-10-03 13:05:12 +0300

    ---

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
+   
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (previous, current) => Math.min(previous, current) );
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (previous, current) => Math.max(previous, current) );
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return array.reduce( (ammount, i) => ammount + i ) / array.length;
 }
